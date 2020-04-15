be2dfc3a7a5342f7b8ee16803c0d999eb501d6af
junki <zou_jun1986@sina.com>
2020-04-15 14:50:50 +0800

    Update intToRoman.py
    add romanToInt

--- intToRoman.py
+++ intToRoman.py
@@ -15,6 +15,28 @@ class Solution(object):
                 result += dic1[index]
             index += 1
         return result
+    
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic1 = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        dic2 = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+        result = 0
+        index = 0
+        pos = 0
+        for x in s:
+            if s[pos:pos+2] in dic1:
+                index = dic1.index(s[pos:pos+2])
+                result += dic2[index]
+                pos += 2
+            elif s[pos:pos+1] in dic1:
+                index = dic1.index(s[pos:pos+1])
+                result += dic2[index]
+                pos += 1
+        return result
 
 if __name__ == '__main__':
     a = Solution()
