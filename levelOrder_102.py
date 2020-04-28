1c6cbfe2d4b6c8ba05621b254ec2deba48a8c8a1
junki <zou_jun1986@sina.com>
2020-04-28 12:05:37 +0800

    Create levelOrder_102.py

---
+++ levelOrder_102.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        result = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            cur = []
+            length = len(queue)
+            for _ in range(length):
+                node = queue.popleft()
+                cur.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(cur)
+        
+        return result
