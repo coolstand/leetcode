e48ddbbabdf791768f71dda6e92aa4d19d6d1935
junki <zou_jun1986@sina.com>
2020-04-28 12:13:04 +0800

    Create levelOrderBottom_107.py

---
+++ levelOrderBottom_107.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        tmp = []
+        result = []
+
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            cur = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                cur.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            tmp.append(cur)
+        
+        for _ in range(len(tmp)):
+            result.append(tmp.pop())
+        
+        return result
